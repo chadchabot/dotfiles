f05446f6399fa171f66cb7dc71eb154368d20a06
Chad Chabot <chadchabot@gmail.com>
2015-06-12 00:45:58 -0400

    Pathogen installed as part of bootstrap phase. Pretty cool.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,8 +5,10 @@
 set -e
 
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-BUNDLE_DIR="${BASE_DIR}/.vim/bundle"
-BACKUP_DIR="${BASE_DIR}/.vim/backup"
+BUNDLE_DIR="${BASE_DIR}/vim/bundle"
+BACKUP_DIR="${BASE_DIR}/vim/backup"
+AUTOLOAD_DIR="${BASE_DIR}/vim/autoload"
+
 MY_NAME="$(whoami)"
 
 #todo
@@ -20,15 +22,27 @@ MY_NAME="$(whoami)"
 #     a mac in the near term, though using VMs is a big part of my work, so worth
 #     considering
 
-#vim
+#vim dir setup
+if [ -f ~/.vim ]; then
+  mv ~/.vim ~/.vim-old
+fi
+echo "Setting up directories"
 [ -d $BACKUP_DIR ] || mkdir -p $BACKUP_DIR
 [ -d $BUNDLE_DIR ] || mkdir -p $BUNDLE_DIR
+[ -d $AUTOLOAD_DIR ] || mkdir -p $AUTOLOAD_DIR
 
 ln -Fs ${BASE_DIR}/vimrc ~/.vimrc #this is hella basic, but should suffice for now
 # I don't want to symlink this dir, I want to create and do the installation of stuff on this machine, not carry around the config... right?
-#ln -Fs ${BASE_DIR}/vim/ ~/.vim #need to make sure I have this set up properly. will have to try in a VM
+# clear out /.vim/folder
+ln -Fs ${BASE_DIR}/vim/ ~/.vim
 
-#now time to install the submodules on to the new host
+#install pathogen
+echo "Downloading pathogen..."
+PATHOGEN_DEST="${AUTOLOAD_DIR}/pathogen.vim"
+curl -LSso $PATHOGEN_DEST https://tpo.pe/pathogen.vim #I should totally be checking for completion of this
+echo "Pathogen download complete"
+
+#now time to install any other fancy submodules, like nerdtree
 pushd ${BASE_DIR}
 echo "$(pwd)"
 popd
@@ -37,17 +51,17 @@ popd
 #ln -s ${BASE_DIR}/gitconfig ~/.gitconfig #guess this means I'm adding my gitconfig next
 
 #terminal prefs (what's the difference between input and bash_profile prefs?)
-if [ -f ~/.inputrc ]
-then
+if [ -f ~/.inputrc ]; then
   mv ~/.inputrc ~/.inputrc-old
 fi
 ln -Fs ${BASE_DIR}/inputrc ~/.inputrc
 
-PLATFORM="$(uname -s)"
 
 # determine the platform you're using, which will determine whether we use
 # homebrew or apt-get (in a far off universe where I may possibly choose a Ubuntu machine again)
-case $PLATFORM in
+
+PLATFORM="$(uname -s)"
+case "$(uname -s)" in
   "Darwin")
     echo "You're running OS X. This is good."
     echo "Soon this will install homebrew for you"
