03b559f593ac65293583987dca28e4581ec350dc
Chad Chabot <chadchabot@gmail.com>
2015-11-09 20:31:20 -0500

    Homebrew casks are working smashingly!

--- lib.sh
+++ lib.sh
@@ -108,11 +108,31 @@ function install_brews() {
 }
 
 function install_casks() {
-  declare -a cask_packages=("vlc" "google-chrome" "firefox" "iterm2" "carbon-copy-cloner" "spectacle" "neovim/neovim" "spotify")
+  declare -a cask_packages=("vlc" "google-chrome" "firefox" "iterm2" "carbon-copy-cloner" "spectacle" "spotify")
   for package in "${cask_packages[@]}"
   do
-    brew cask install "${package}"
+    install_brew_cask "${package}"
   done
+}
+
+function install_brew_cask() {
+  CASK_NAME=`echo "${1}"`
+  #TODO: look in both the installed brew casks as well as in
+  #      the /Applications folder
+
+  if cask_is_installed ${CASK_NAME}; then
+    ok "${CASK_NAME} is already installed"
+  else
+    brew cask install "${CASK_NAME}"
+  fi
+}
+
+function cask_is_installed() {
+  if brew cask list | grep -q ${1}; then
+    return 0
+  else
+    return 1
+  fi
 }
 
 function install_brew_package() {
