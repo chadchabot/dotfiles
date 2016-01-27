4cfc6e3171571b9dc0bb876466302ddbc7d4d87e
Chad Chabot <chadchabot@gmail.com>
2016-01-26 22:45:48 -0500

    Adds dropbox to Homebrew downloads

--- osx/homebrew.sh
+++ osx/homebrew.sh
@@ -14,7 +14,7 @@ function install_brews() {
 }
 
 function install_casks() {
-  declare -a dev_essentials=("sublime-text" "google-chrome" "firefox" "iterm2" "spectacle" "spotify" "caffeine" "slack" "sequel-pro")
+  declare -a dev_essentials=("sublime-text" "google-chrome" "firefox" "iterm2" "spectacle" "spotify" "caffeine" "slack" "sequel-pro" "dropbox")
   declare -a personal=("superduper" "vlc" "carbon-copy-cloner" "flux" "little-snitch" "xscope" "transmit" "evernote" "skype" "handbrake" "mou" "steam" "1Password")
 
   for package in "${dev_essentials[@]}"
