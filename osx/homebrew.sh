183a0e048eb1c7127369a8a10d75eae65e6dac33
Chad Chabot <chadchabot@gmail.com>
2016-03-02 15:22:18 -0500

    Add SQLite browser to Cask list

--- osx/homebrew.sh
+++ osx/homebrew.sh
@@ -14,7 +14,7 @@ function install_brews() {
 }
 
 function install_casks() {
-  declare -a dev_essentials=("sublime-text" "google-chrome" "firefox" "iterm2" "spectacle" "spotify" "caffeine" "slack" "sequel-pro" "dropbox")
+  declare -a dev_essentials=("sublime-text" "google-chrome" "firefox" "iterm2" "spectacle" "spotify" "caffeine" "slack" "sequel-pro" "dropbox" "sqlitebrowser")
   declare -a personal=("superduper" "vlc" "carbon-copy-cloner" "flux" "little-snitch" "xscope" "transmit" "evernote" "skype" "handbrake" "mou" "steam" "1password")
 
   for package in "${dev_essentials[@]}"
